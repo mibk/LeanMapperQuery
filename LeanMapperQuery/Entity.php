82107af5e9e432619b4ba378135811c200323b3a
Jan Pecha <janpecha@email.cz>
2018-07-19 09:17:05 +0200

    Entity: fix for Single Table Inheritance
    (cherry picked from commit 0c97f36)

--- LeanMapperQuery/Entity.php
+++ LeanMapperQuery/Entity.php
@@ -86,8 +86,9 @@ class Entity extends LeanMapper\Entity
 			throw new InvalidRelationshipException('Only BelongsToMany and HasMany relationships are supported when querying entity property. ' . get_class($relationship) . ' given.');
 		}
 		$entities = array();
+		$table = $mapper->getTable($class);
 		foreach ($rows as $row) {
-			$entity = $this->entityFactory->createEntity($class, $row);
+			$entity = $this->entityFactory->createEntity($mapper->getEntityClass($table, $row), $row);
 			$entity->makeAlive($this->entityFactory);
 			$entities[] = $entity;
 		}
